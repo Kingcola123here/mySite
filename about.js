ad7a5f48a823a2caf46c0aceb81d98863078ce2c
Kingcola123here <119832527+Kingcola123here@users.noreply.github.com>
2024-02-28 14:53:51 +0545

    Add files via upload
    added index

---
+++ about.js
@@ -0,0 +1,555 @@
+
+function closeoverlay(){
+    document.getElementById('overlay').style.display = 'none';
+  }
+  
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    x.classList.toggle("responsive");
+  }
+  
+  
+  const slidersProducts = {
+      hotdeals: [
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/1-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/1-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/1-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/1-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        }
+      ],
+      productsnew: [
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/1-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/2-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/3-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/4-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/5-1.png",
+          oldPrice: "Rs. 2000",
+          newPrice: "Rs. 1000",
+          rating: 5
+        },
+        // Add more products here as needed
+      ],
+      productsonsale: [
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/1-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/2-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/3-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/4-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/5-1.png",
+          oldPrice: "Rs. 2000",
+          newPrice: "Rs. 1000",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK ",
+          imageSrc: "./products/Men/1-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/2-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK ",
+          imageSrc: "./products/Men/3-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK ",
+          imageSrc: "./products/Men/4-1.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK ",
+          imageSrc: "./products/Men/5-1.png",
+          oldPrice: "Rs. 2000",
+          newPrice: "Rs. 1000",
+          rating: 5
+        },
+        // Add more products here as needed
+      ],
+      testimonial: [
+        {
+          name:"Asaroth",
+          imageSrc:"./testimonial/testimonial1.jpg"
+          },
+        {
+          name:"Shakira",
+          imageSrc:"./testimonial/testimonial2.jpg"
+        },
+     
+          {
+            name:"Haryy",
+            imageSrc:"./testimonial/testimonial3.jpg"
+          },
+      ],
+      blog: [
+        {
+          name:"RECIPIE OF PERFRUME",
+          imageSrc:"./blog/blog1.jpg"
+          },
+        {
+          name:"ROSE PERFUME",
+          imageSrc:"./blog/blog2.jpg"
+        },
+     
+        {
+          name:"PERFUME WORLD",
+          imageSrc:"./blog/blog3.jpg"
+        },
+        {
+          name:"PERFUME FUN",
+          imageSrc:"./blog/blog4.jpg"
+        },
+      ],
+      products: [
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/1-1.png",
+          imageSrc2: "./products/Men/1-2.png",
+          imageSrc3: "./products/Men/1-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/2-1.png",
+          imageSrc2: "./products/Men/2-2.png",
+          imageSrc3: "./products/Men/2-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/3-1.png",
+          imageSrc2: "./products/Men/3-2.png",
+          imageSrc3: "./products/Men/3-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/4-1.png",
+          imageSrc2: "./products/Men/4-2.png",
+          imageSrc3: "./products/Men/4-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/5-1.png",
+          imageSrc2: "./products/Men/5-1.png",
+          imageSrc3: "./products/Men/5-1.png",
+          oldPrice: "Rs. 2000",
+          newPrice: "Rs. 1000",
+          rating: 5
+        },
+        // Add more products here as needed
+      ],
+      products1: [
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/1-1.png",
+          imageSrc2: "./products/Men/1-2.png",
+          imageSrc3: "./products/Men/1-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/2-1.png",
+          imageSrc2: "./products/Men/2-2.png",
+          imageSrc3: "./products/Men/2-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/3-1.png",
+          imageSrc2: "./products/Men/3-2.png",
+          imageSrc3: "./products/Men/3-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/4-1.png",
+          imageSrc2: "./products/Men/4-2.png",
+          imageSrc3: "./products/Men/4-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+     
+        // Add more products here as needed
+      ],
+      products2: [
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/1-1.png",
+          imageSrc2: "./products/Men/1-2.png",
+          imageSrc3: "./products/Men/1-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/2-1.png",
+          imageSrc2: "./products/Men/2-2.png",
+          imageSrc3: "./products/Men/2-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/3-1.png",
+          imageSrc2: "./products/Men/3-2.png",
+          imageSrc3: "./products/Men/3-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/4-1.png",
+          imageSrc2: "./products/Men/4-2.png",
+          imageSrc3: "./products/Men/4-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/5-1.png",
+          imageSrc2: "./products/Men/5-1.png",
+          imageSrc3: "./products/Men/5-1.png",
+          oldPrice: "Rs. 2000",
+          newPrice: "Rs. 1000",
+          rating: 5
+        },
+        // Add more products here as needed
+      ],
+      products3: [
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/1-1.png",
+          imageSrc2: "./products/Men/1-2.png",
+          imageSrc3: "./products/Men/1-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/2-1.png",
+          imageSrc2: "./products/Men/2-2.png",
+          imageSrc3: "./products/Men/2-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/3-1.png",
+          imageSrc2: "./products/Men/3-2.png",
+          imageSrc3: "./products/Men/3-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+        {
+          name: "JAGUAR CLASSIC BLACK",
+          imageSrc: "./products/Men/4-1.png",
+          imageSrc2: "./products/Men/4-2.png",
+          imageSrc3: "./products/Men/4-2.png",
+          oldPrice: "Rs. 3300",
+          newPrice: "Rs. 2046",
+          rating: 5
+        },
+     
+        // Add more products here as needed
+      ],
+    
+      productbestseller : [
+        {
+          name: "Dolce & Gabbana Women",
+          image: "./best-product/B1-1.png",
+          rating: 5,
+          price: 9000,
+          oldPrice: 1000,
+        },
+        {
+    
+          name: "Jean Paul Gaultier",
+          image: "./best-product/B2-1.png",
+          rating: 5,
+          price: 7999,
+          oldPrice: 1000,
+       
+    
+        },
+        {
+          name: "Carolina Herrera Women 212 VIP",
+          image: "./best-product/B3-1.png",
+          rating: 5,
+          price: 7499,
+          oldPrice: 1000
+        },
+        {
+          name: "Dolce & Gabbana Women",
+          image: "./best-product/B1-1.png",
+          rating: 5,
+          price: 9000,
+          oldPrice: 1000,
+        },
+        {
+    
+          name: "Jean Paul Gaultier",
+          image: "./best-product/B2-1.png",
+          rating: 5,
+          price: 7999,
+          oldPrice: 1000,
+       
+    
+        },
+        {
+          name: "Carolina Herrera Women 212 VIP",
+          image: "./best-product/B3-1.png",
+          rating: 5,
+          price: 7499,
+          oldPrice: 1000
+        },
+        {
+          name: "Dolce & Gabbana Women",
+          image: "./best-product/B1-1.png",
+          rating: 5,
+          price: 9000,
+          oldPrice: 1000,
+        },
+        {
+    
+          name: "Jean Paul Gaultier",
+          image: "./best-product/B2-1.png",
+          rating: 5,
+          price: 7999,
+          oldPrice: 1000,
+       
+    
+        },
+        {
+          name: "Carolina Herrera Women 212 VIP",
+          image: "./best-product/B3-1.png",
+          rating: 5,
+          price: 7499,
+          oldPrice: 1000
+        },
+      ],
+      
+      
+      // Add more sliders with their respective products
+    };
+  
+  jQuery(function($) {
+      $('.sliderfeatureproducts').slick({
+        slidesToShow: 1,
+        rows:3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 769,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              rows:3,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+    
+    });
+  
+    jQuery(function($) {
+      $('.slidermostviewproducts').slick({
+        slidesToShow: 1,
+        rows:3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 769,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              rows:3,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+    
+    });
+    let outPutfeaturedproducts="";
+  slidersProducts.productbestseller.forEach((product, index) => {
+    // Create a div element for the product
+  
+  
+    // Generate HTML for the product
+  outPutfeaturedproducts += `
+  <div class="product-listing">
+    <div class="product-card" style="margin-top:.6rem;">
+      <img src="${product.image}" alt="${product.name}" class="product-image">
+          <div class="productinfohere">
+            <div class="product-title">${product.name}</div>
+            <div class="product-rating">
+              ${'★'.repeat(product.rating)}
+            </div>
+            <div class="product-price">Rs. ${product.price} <span class="product-price-old1">Rs. ${product.oldPrice}</span></div>
+          </div>
+    </div>
+  </div>
+    `;
+  
+    // Append the product HTML to the product slider container
+    document.getElementById("product-sliderfeaturedproducts").innerHTML = outPutfeaturedproducts;
+  });
+  
+  
+  
+  let outPutmostviewproducts="";
+  slidersProducts.productbestseller.forEach((product, index) => {
+    // Create a div element for the product
+  
+  
+    // Generate HTML for the product
+  outPutmostviewproducts += `
+  <div class="product-listing">
+    <div class="product-card" style="margin-top:.6rem;">
+      <img src="${product.image}" alt="${product.name}" class="product-image">
+          <div class="productinfohere">
+            <div class="product-title">${product.name}</div>
+            <div class="product-rating">
+              ${'★'.repeat(product.rating)}
+            </div>
+            <div class="product-price">Rs. ${product.price} <span class="product-price-old1">Rs. ${product.oldPrice}</span></div>
+          </div>
+    </div>
+  </div>
+    `;
+  
+    // Append the product HTML to the product slider container
+    document.getElementById("product-slidermostviewproducts").innerHTML = outPutmostviewproducts;
+  });
+  
+    
